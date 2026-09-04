af7d956125d173749dd988b983ad387e38c4315e
Landrykb <landrykassa@gmail.com>
2026-09-04 21:59:38 +0900

    Add social portfolio — BleepxQuery

---
+++ social/capstone_social/query.sql
@@ -0,0 +1,16 @@
+WITH TweetStats AS (
+  SELECT user_id, 
+  COUNT(*) AS tweet_count 
+  FROM tweets 
+  GROUP BY user_id
+),
+UserStats AS (
+  SELECT user_id, user_name FROM users
+)
+SELECT 
+  us.user_name,
+  ts.tweet_count
+  FROM TweetStats ts 
+  JOIN UserStats us ON ts.user_id = us.user_id
+ORDER BY tweet_count DESC;
+
